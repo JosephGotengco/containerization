3e71166d6b4d83d2c81eabaeaf844970f8be5416
Joseph Gotengco <josephgotnco@gmail.com>
2020-11-05 14:55:02 -0800

    lab8

--- storage/drop_tables_mysql.py
+++ storage/drop_tables_mysql.py
@@ -1,7 +1,7 @@
 import mysql.connector
 
 db_conn = mysql.connector.connect(host="kafka-service-based.westus2.cloudapp.azure.com", user="root",
-                                  password="PASSWORD HERE", database="events")
+                                  password="password", database="events")
 
 db_cursor = db_conn.cursor()
 
